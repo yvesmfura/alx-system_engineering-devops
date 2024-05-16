e7cd08491b5e3a697f491c5fecfe1d837add8b29
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-05-16 12:39:24 +0200

    holberton user and open a file without any error message

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # Change the OS configuration so that it is possible to login with the
-# holberton user and open a file without any error message.
+# holberton user and open a file without any error message
 
 exec {'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
